f026a49a6ddae51f4acb2fc6d798fd986f5543be
lrsm21427 <liurui@mails.swust.edu.cn>
2025-01-05 19:26:20 +0800

    初始创建v1.0.0

---
+++ .vitepress/theme/assets/linkData.mjs
@@ -0,0 +1,33 @@
+// 链接数据
+const linkData = [
+  // 推荐数据
+  {
+    type: "rec",
+    typeName: "推荐",
+    typeDesc: "都是大佬，推荐关注",
+    typeList: [
+      {
+        name: "xxx",
+        avatar: "",
+        desc: "独立开发者",
+        url: "",
+      },
+    ],
+  },
+  // 小伙伴们
+  {
+    type: "friends",
+    typeName: "小伙伴们",
+    typeDesc: "我们在一起，共同进步",
+    typeList: [
+      {
+        name: "R",
+        avatar: "/images/logo/logo.png",
+        desc: "分享技术与科技生活",
+        url: "",
+      },
+    ],
+  },
+];
+
+export default linkData;
